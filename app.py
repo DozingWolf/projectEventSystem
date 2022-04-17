85e9b8f75355a86156e1e2598355f7641f67c515
陈圣俞 <chenshengyu1@sinopharm.com>
2022-04-17 19:49:52 +0800

    增加新的蓝图

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from konfig import Config
 from model import db
 from controller.helloPage import helloBP
 from controller.getData import getDataBP
+from controller.postData import postDataBP
 # load parameter
 confFile = Config('./conf/para.conf')
 # initial flask
@@ -11,6 +12,7 @@ app.config.update(confFile.get_map('flask'))
 # register blueprint
 app.register_blueprint(helloBP,url_prefix='/api/v1.0')
 app.register_blueprint(getDataBP,url_prefix='/api/v1.0/getdata')
+app.register_blueprint(postDataBP,url_prefix='/api/v1.0/postdata')
 # initial db
 db.init_app(app)
 
