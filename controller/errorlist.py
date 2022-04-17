1b3836d47d71fd51d7bdfa3f4299d2c76eaeb1cf
陈圣俞 <chenshengyu1@sinopharm.com>
2022-04-17 19:50:07 +0800

    增加自定义error类型

--- controller/errorlist.py
+++ controller/errorlist.py
@@ -55,21 +55,61 @@ class IntfNoParaError(IntfError):
         self.message = message
         self.status = status
 
-class InnerError(Exception):
-    def __init__(self, message, status=-1):
-        super().__init__(message, status)
-        self.message = message
-        self.status = status
-
-class InnerParaError(InnerError):
-    def __init__(self, message, status=-1):
-        super().__init__(message, status)
-        self.message = message
-        self.status = status
-
 class TimerError(Exception):
     def __init__(self, message, status=-1):
         super().__init__(message, status)
         self.message = message
         self.status = status
 
+class InnerArgumentError(Exception):
+    code = '520'
+    error_code = 1500
+    def __init__(self, message, arguname, status=-1):
+        super().__init__(message, arguname, status)
+        self.message = message
+        self.status = status
+        self.arguname = arguname
+
+class InnerArgumentInputError(InnerArgumentError):
+    code = '520'
+    error_code = 1501
+    def __init__(self, message, arguname, status=-1):
+        super().__init__(message, arguname, status)
+        self.message = message
+        self.status = status
+        self.arguname = arguname
+
+class InnerArgumentOutputError(InnerArgumentError):
+    code = '520'
+    error_code = 1502
+    def __init__(self, message, arguname, status=-1):
+        super().__init__(message, arguname, status)
+        self.message = message
+        self.status = status
+        self.arguname = arguname
+
+class PostParaError(Exception):
+    code = '521'
+    error_code = '1600'
+    def __init__(self, message, arguname, status=-1):
+        super().__init__(message, arguname, status)
+        self.message = message
+        self.status = status
+        self.arguname = arguname
+
+class PostNoParaError(PostParaError):
+    code = '521'
+    error_code = '1601'
+    def __init__(self, message, arguname, status=-1):
+        super().__init__(message, arguname, status)
+        self.message = message
+        self.status = status
+        self.arguname = arguname
+
+class PostParaEmptyError(PostParaError):
+    error_code = '1602'
+    def __init__(self, message, arguname, status=-1):
+        super().__init__(message, arguname, status)
+        self.message = message
+        self.status = status
+        self.arguname = arguname
