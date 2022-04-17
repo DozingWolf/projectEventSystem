3eaaaeb84294fae37642db4563e21329d308ebd8
陈圣俞 <chenshengyu1@sinopharm.com>
2022-04-17 21:25:17 +0800

    增加项目成员新增接口

--- controller/postData.py
+++ controller/postData.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Blueprint,current_app,request,make_response,g
 from sqlalchemy import exc
 from model import db
@@ -335,6 +336,85 @@ def postCreateEvent():
             current_app.logger.info('No query para input')
             raise PostNoParaError(message='post request havent json payload',
                                   arguname='projectid, eventtime, eventcreationid, eventstatus, eventmsg, createuserid')
+        current_app.logger.debug('===================================')
+        current_app.logger.debug(insertArg)
+        current_app.logger.debug('===================================')
+    except PostParaEmptyError as empvalue:
+        return responseStructures(rstatus=empvalue.code,
+                                  rbody={'error_code':empvalue.error_code,
+                                         'error_msg':empvalue.message,
+                                         'args':empvalue.arguname})
+    except PostNoParaError as nopara:
+        return responseStructures(rstatus=nopara.code,
+                                  rbody={'error_code':nopara.error_code,
+                                         'error_msg':nopara.message,
+                                         'args':nopara.arguname})
+    except Exception as err:
+        current_app.logger.error('Unknow Error when analyze post requests para, please chack log file to find detail error message')
+        current_app.logger.error(err)
+        return responseStructures(rstatus='521',
+                                  rbody={'error_code':1699,
+                                         'error_msg':'Unknow Error when analyze post requests para, please chack log file to find detail error message',
+                                         'args':''})
+    # 记录insert数据
+    current_app.logger.debug(insertSQL)
+    current_app.logger.debug(insertArg)
+    # 执行查询
+    try:
+        exeSQL = db.session.execute(insertSQL,insertArg)
+        db.session.commit()
+    except exc.SQLAlchemyError as err:
+        current_app.logger.error('SQLAlchemyError, please check log file to find detail error message')
+        current_app.logger.error(err)
+        return responseStructures(rstatus='522',
+                                  rbody={'error_code':'1501',
+                                         'error_msg':'SQLAlchemyError, please check log file to find detail error message',
+                                         'args':''})
+    except Exception as err:
+        current_app.logger.error('something was wrong, db will rollback this transaction data ')
+        current_app.logger.error(err)
+        db.session.rollback()
+        return responseStructures(rstatus='522',
+                                  rbody={'error_code':'1599',
+                                         'error_msg':'DB Unknow Error, data have been rollback, please check log file to find detail error message',
+                                         'args':''})
+    # 处理数据成为json
+    try:
+        returnData = {'error_code':'2000','error_msg':'insert success','dataset':'None'}
+        return responseStructures(rstatus='200',
+                                  rbody=returnData)
+    except Exception as err:
+        current_app.logger.error('something was wrong, db have been inserted into db, but make response was failed, please check log file to find detail error message')
+        current_app.logger.debug(err)
+        return responseStructures(rstatus='522',
+                                  rbody={'error_code':'1599',
+                                         'error_msg':'Server Unknow Error, data have been inserted into db, please check log file to find detail error message',
+                                         'args':''})
+
+
+@postDataBP.route('/postCreatePrjMember',methods=['POST'])
+def postCreatePrjMember():
+# 新增项目成员接口
+    # 标准SQL和变量定义
+    insertSQL = '''
+            insert into edm_test_schema.trltprjmember 
+            (projectid, userid, memberstatus, createuserid)
+            values 
+            (:projectid, :userid, :memberstatus, :createuserid)
+    '''
+    insertArg = {'createuserid':0}
+    argList = [('projectid',1),('userid',1),('memberstatus',1)]
+    # 获取用户传递的变量数据并判断 
+    try:
+        # 获取json传递的变量
+        inputPara = request.get_json()
+        if inputPara is not None:
+            # 处理需要插入的数据
+            insertArg = insertSqlParaGenerator(rpara=inputPara,isexist=argList,defaultinsarg=insertArg)
+        else:
+            current_app.logger.info('No query para input')
+            raise PostNoParaError(message='post request havent json payload',
+                                  arguname='projectid, userid, memberstatus')
         current_app.logger.debug('===================================')
         current_app.logger.debug(insertArg)
         current_app.logger.debug('===================================')
