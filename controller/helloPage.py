5677c6249e87e5f3cd20037fccf0d297261b25c6
陈圣俞 <chenshengyu1@sinopharm.com>
2022-04-20 21:25:23 +0800

    测试登入校验

--- controller/helloPage.py
+++ controller/helloPage.py
@@ -1,7 +1,9 @@
 from flask import Blueprint,current_app
+from auth.authManager import isLoginCheck
 
 helloBP = Blueprint('helloPage',__name__)
 
 @helloBP.route('/hello',methods=['GET'])
+@isLoginCheck
 def hello():
     return 'Hello World!'
