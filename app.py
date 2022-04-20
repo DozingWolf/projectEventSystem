94be0510c2369c00c3c0f11fed43aef293e77e5e
陈圣俞 <chenshengyu1@sinopharm.com>
2022-04-20 16:36:45 +0800

    增加蓝图

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from konfig import Config
 from model import db
+from auth.authManager import authManagerBP
 from controller.helloPage import helloBP
 from controller.getData import getDataBP
 from controller.postData import postDataBP
@@ -11,6 +12,7 @@ app = Flask(__name__)
 app.config.update(confFile.get_map('flask'))
 # register blueprint
 app.register_blueprint(helloBP,url_prefix='/api/v1.0')
+app.register_blueprint(authManagerBP,url_prefix='/api/v1.0/auth')
 app.register_blueprint(getDataBP,url_prefix='/api/v1.0/getdata')
 app.register_blueprint(postDataBP,url_prefix='/api/v1.0/postdata')
 # initial db
