3cac78f879921558f9510c758b09dae9c3f18aa9
陈圣俞 <chenshengyu1@sinopharm.com>
2022-04-20 21:26:04 +0800

    增加用户对象
    用户对象未来运用到g对象中，先放着

--- model/user.py
+++ model/user.py
@@ -1,13 +1,23 @@
 from flask import current_app
 
 class User():
-    def __init__(self, userid, username, pcode) -> None:
-        self.userid = userid
-        self.username = username
-        self.permissioncode = pcode
+    def __init__(self, userid, username, pcode, logintime) -> None:
+        self.__userid = userid
+        self.__username = username
+        self.__permissioncode = pcode
+        self.__logintime = logintime
+        self.__is_login = False
     def getUserID(self):
-        return self.userid
+        return self.__userid
     def getUserName(self):
-        return self.username
+        return self.__username
     def getUserPermissions(self):
-        return self.permissioncode
+        return self.__permissioncode
+    def isLogin(self):
+        self.__is_login = True
+        return self.__is_login
+    def isLogout(self):
+        self.__is_login = False
+        return self.__is_login
+    def getLoginTime(self):
+        return self.__logintime
