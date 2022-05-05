3574676e83ca9e197f719bb7f012f48ed333b7f3
DozingWolf <43899290+DozingWolf@users.noreply.github.com>
2022-05-05 11:16:57 +0800

    修改认证出错的http code

--- controller/errorlist.py
+++ controller/errorlist.py
@@ -62,6 +62,7 @@ class AuthDupUserError(AuthError):
         self.arguname = arguname
 
 class AuthNoPermissionError(AuthError):
+    code = '403'
     error_code = '1404'
     def __init__(self, message, arguname, status=-1):
         super().__init__(message, status)
